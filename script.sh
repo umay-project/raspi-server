27abd57a824967102a9188af69a9fc68479f73c7
ER-Mustafa <mer20@ku.edu.tr>
2024-12-01 16:58:54 +0300

    scripts better now

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 while true; 
 
-do   arecord -D plughw:2,0 -d 5 -f cd -t wav | curl -X POST -H "Content-Type: audio/wav" --data-binary @- http://umay.develop-er.org/upload-audio;
-
-sleep 1
+do arecord -D plughw:2,0 -d 5 -f cd -t wav | \
+       	curl -X POST -H "Content-Type: audio/wav" --data-binary @- http://umay.develop-er.org/upload-audio &
+sleep 6
 
 done
